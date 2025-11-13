325c341580289ea27895854fe26423af82f74783
mando0046 <121170038+mando0046@users.noreply.github.com>
2025-11-13 21:22:04 +0800

    first commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,49 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/utama', function () {
+    return view('utama',["title"=>"Utama"]);
+
+});
+
+
+Route::get('/home', function () {
+    return view('home',[
+        "title"=>"Home"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+"title"=>"Biodata",
+"name"=>"Azwa Nisyiatul Rizky",
+"email"=> "azwa_nisya@gmail.com",
+"image"=>"azwa.jpg"
+
+    ]);
+});
+
+
+Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('posts/{post:slug}', [PostController::class,'show']);
+
+Route::get('/categories/{category:slug}',  function (Category $category) {
+        return view('posts',[
+            'title'=> "Post by Category : $category->name",
+            'posts'=> $category->posts->load('category','author')
+            
+        ] );
+        
+        });
+
+        Route::get('/authors/{author:username}',  function (User $author) {
+        return view('posts',[
+            'title'=> 'User posts',
+            'posts'=> $author->post->load('category', 'author'),
+          
+        ] );
+        
+    });
+
+
